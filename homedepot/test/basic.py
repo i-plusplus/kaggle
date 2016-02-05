0e583b431748e642b3ff4326658335d333308089
paras.mal <paras.mal@directi.com>
2016-02-06 02:04:29 +0530

    adding count of numbers to features

--- homedepot/test/basic.py
+++ homedepot/test/basic.py
@@ -9,31 +9,43 @@ import datetime
 from xgboost.sklearn import XGBRegressor
 from sklearn import grid_search, linear_model
 from sklearn.linear_model import LinearRegression
+import os
 stemmer = SnowballStemmer('english')
 
 df_train_x = pd.read_csv('train_x.csv', encoding="ISO-8859-1")
 df_test_x = pd.read_csv('test_x.csv', encoding="ISO-8859-1")
 
-df_train_x = df_train_x.drop(['search_term_nouns','product_title_nouns','product_description_nouns','search_term_stripped','product_title_stripped','product_description_stripped','search_term','product_title','product_description'],axis=1)
+df_train_x = df_train_x.drop(['cc','search_term_nouns','product_title_nouns','product_description_nouns','search_term_stripped','product_title_stripped','product_description_stripped','search_term','product_title','product_description'],axis=1)
 
-df_test_x = df_test_x.drop(['search_term_nouns','product_title_nouns','product_description_nouns','search_term_stripped','product_title_stripped','product_description_stripped','search_term','product_title','product_description'],axis=1)
+df_test_x = df_test_x.drop(['cc','search_term_nouns','product_title_nouns','product_description_nouns','search_term_stripped','product_title_stripped','product_description_stripped','search_term','product_title','product_description'],axis=1)
 
-df_train_x.to_csv("/tmp/train.csv")
-
+train_old = df_train_x
+test_old = df_test_x
 id_test = pd.read_csv('test_id.csv')
 
 y_train = pd.read_csv('train_y.csv')
-
 xgb = XGBRegressor(objective='reg:linear', subsample=1.0, colsample_bytree=0.5, seed=0)                  
-parameters = {'max_depth' : [6], 'learning_rate':[.3], 'n_estimators':[40]}
+parameters = {'max_depth' : [6], 'learning_rate':[.05], 'n_estimators':[160]}
 clf = grid_search.GridSearchCV(xgb, parameters)
 clf.fit(df_train_x.values, y_train['relevance'].values)
-
 y_pred = clf.predict(df_test_x)
-
 id_test['relevance'] = [max(x,1) for x in y_pred]
 id_test['relevance'] = [min(x,3) for x in id_test['relevance'].values]
-
-
 id_test.to_csv('submission.csv',index=False)
+os.system("python3 checkResults.py submission.csv")
+'''
+for x in train_old.columns.values:
+    df_train_x = train_old.drop([x],axis = 1)
+    df_test_x = test_old.drop([x],axis = 1)
+    xgb = XGBRegressor(objective='reg:linear', subsample=1.0, colsample_bytree=0.5, seed=0)                  
+    parameters = {'max_depth' : [6], 'learning_rate':[.05], 'n_estimators':[160]}
+    clf = grid_search.GridSearchCV(xgb, parameters)
+    clf.fit(df_train_x.values, y_train['relevance'].values)
+    y_pred = clf.predict(df_test_x)
+    id_test['relevance'] = [max(x,1) for x in y_pred]
+    id_test['relevance'] = [min(x,3) for x in id_test['relevance'].values]
+    id_test.to_csv('submission.csv',index=False)
+    print(x)
+    os.system("python3 checkResults.py submission.csv")
+'''
 
--- homedepot/test/checkDigits.py
+++ homedepot/test/checkDigits.py
@@ -6,14 +6,36 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from nltk.tag import pos_tag
 
+stop = stopwords.words('english')
 non_decimal = re.compile(r'[^\d.]+')
 train = pd.read_csv("train_x.csv", encoding="ISO-8859-1")
 test = pd.read_csv("test_x.csv", encoding="ISO-8859-1")
 df_all = pd.concat((train,test), axis = 0)
 
+colors = ['black','white']
+
+def common_color(str1):
+    query = str(str1[0]).lower().split()
+    value = str(str1[1]).lower().split()
+    query = [x for x in query if x in colors]
+    value = [x for x in value if x in colors]
+    return len([x for x in query if x in value])
+
+
 def str_common_word(str1, str2):
-    x = [x for x in str(str1).lower().split() if str(str2).lower().find(x)>=0 ]
+    x = [x for x in str(str1).lower().split() if x in str(str2).lower().split() ]
     return len(x)
+
+def str_common_word2(str1):
+    q = [x for x in str(str1[0]).lower().split() if x not in stop]
+    v = str(str1[1])
+    count = 0
+    for i in range(1, len(q)):
+        p = " " + q[i-1] + " " + q[i] + " "
+        if v.lower().find(p) >= 0:
+            count = count + 1
+    return count
+
 
 def checkdigits(str1):
     query = str(non_decimal.sub(" ", str1[0]))
@@ -22,17 +44,25 @@ def checkdigits(str1):
     value2 = " ".join([x.split(".")[0] for x in value.split()])
     return [str_common_word(query,value),str_common_word(query2,value2)]
 
+def checkdigits2(str1):
+    query = str(non_decimal.sub(" ", str1[0]))
+    query2 = " ".join([x.split(".")[0] for x in query.split()])
+    if len(query.split()) > 0:
+       return len(query.split())
+    return 0
+
 
 df_all[['product_title_digits', 'product_title_digits2']] = df_all[['search_term','product_title']].apply(checkdigits, axis = 1)
 df_all[['product_description_digits','product_description_digits2']] = df_all[['search_term','product_description']].apply(checkdigits, axis = 1)
 
+df_all['search_term_digit_count'] = df_all[['search_term']].apply(checkdigits2, axis = 1)
+df_all['product_title_count'] = df_all[['product_title']].apply(checkdigits2, axis = 1)
+df_all['product_description_count'] = df_all[['product_description']].apply(checkdigits2, axis = 1)
+df_all['cc'] = df_all[['product_description']].apply(checkdigits2, axis = 1)
 
-def checkdigits(str1):
-    query = str(non_decimal.sub(" ", str1[0]))
-    value = str(non_decimal.sub(" ", str1[1]))
-    return str_common_word(query,value)
 
-
+#df_all['product_title_color'] = df_all[['search_term','product_title']].apply(str_common_word2, axis = 1)
+#df_all['product_description_color'] = df_all[['search_term','product_title']].apply(str_common_word2, axis = 1)
 
 train = pd.DataFrame(df_all.values[:train.shape[0]], columns = df_all.columns.values)
 test = pd.DataFrame(df_all.values[train.shape[0]:], columns = df_all.columns.values)
